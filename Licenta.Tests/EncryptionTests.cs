d0a731c6a27acd39d3f28c7df1243317a1a35404
Bianca <Bianca@ASUS>
2017-12-28 20:07:44 +0200

    Add unit test for encryption/decryption

---
+++ Licenta.Tests/EncryptionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using Licenta.ORM;
+
+namespace Licenta.Tests
+{
+    [TestClass]
+    public class EncryptionTests
+    {
+        [TestMethod]
+        public void CanEncryptAndDecryptSuccessfully()
+        {
+            var aes = new Aes();
+
+            string password = "123";
+            string plaintext = "plaintext";
+
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+            byte[] encryptedBytes = aes.Encrypt(plaintextBytes, password);
+
+            byte[] decryptedBytes = aes.Decrypt(encryptedBytes, password);
+            string decryptedString = Encoding.UTF8.GetString(decryptedBytes);
+
+            Assert.AreEqual(plaintext, decryptedString);
+        }
+    }
+}
