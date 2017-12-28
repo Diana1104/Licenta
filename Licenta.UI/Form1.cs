3a3d988a2897ad96277f0a7390d840a01ddcd597
Bianca <Bianca@ASUS>
2017-12-28 18:25:22 +0200

    Remove DbContext since it is no longer necessary

--- Licenta.UI/Form1.cs
+++ Licenta.UI/Form1.cs
@@ -1,21 +1,23 @@
 ﻿using Licenta.Data;
-using System;
+using Licenta.ORM;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace Licenta.UI
 {
     public partial class Form1 : Form
     {
-        DbContext context;
+        Db db;
 
         public Form1()
         {
             InitializeComponent();
 
-            context = new DbContext();
+            var connectionString = ConfigurationManager.ConnectionStrings["Licenta"].ConnectionString;
+            db = new Db(connectionString);
 
-            this.dataGridView1.DataSource = context.GetPersons();
-            this.dataGridView2.DataSource = context.GetProducts();
+            this.dataGridView1.DataSource = db.GetAll<Person>();
+            this.dataGridView2.DataSource = db.GetAll<Product>();
         }
 
         private void button1_Click(object sender, System.EventArgs e)
@@ -24,10 +26,10 @@ namespace Licenta.UI
             var result = editPersonForm.ShowDialog();
             if (result == DialogResult.OK)
             {
-                context.Save(editPersonForm.Person);
+                db.Save(editPersonForm.Person);
             }
 
-            this.dataGridView1.DataSource = context.GetPersons();
+            this.dataGridView1.DataSource = db.GetAll<Person>();
         }
     }
 }
