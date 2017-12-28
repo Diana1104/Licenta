9041af51d59a0bf9f10c352905a8a079ebac9020
Bianca <Bianca@ASUS>
2017-12-28 20:04:04 +0200

    Encrypt/Decrypt bytes instead of strings, for flexibility

--- Licenta.ORM/Aes.cs
+++ Licenta.ORM/Aes.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Text;
-using System.Security.Cryptography;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace Licenta.ORM
 {
-    public static class StringCipher
+    public class Aes
     {
         // This constant is used to determine the keysize of the encryption algorithm in bits.
         // We divide this by 8 within the code below to get the equivalent number of bytes.
@@ -15,13 +14,12 @@ namespace Licenta.ORM
         // This constant determines the number of iterations for the password bytes generation function.
         private const int DerivationIterations = 1000;
 
-        public static string Encrypt(string plainText, string passPhrase)
+        public byte[] Encrypt(byte[] plainTextBytes, string passPhrase)
         {
             // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
             // so that the same Salt and IV values can be used when decrypting.  
             var saltStringBytes = Generate256BitsOfRandomEntropy();
             var ivStringBytes = Generate256BitsOfRandomEntropy();
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
             using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
             {
                 var keyBytes = password.GetBytes(Keysize / 8);
@@ -44,7 +42,7 @@ namespace Licenta.ORM
                                 cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
                                 memoryStream.Close();
                                 cryptoStream.Close();
-                                return Convert.ToBase64String(cipherTextBytes);
+                                return cipherTextBytes;
                             }
                         }
                     }
@@ -52,11 +50,8 @@ namespace Licenta.ORM
             }
         }
 
-        public static string Decrypt(string cipherText, string passPhrase)
+        public byte[] Decrypt(byte[] cipherTextBytesWithSaltAndIv, string passPhrase)
         {
-            // Get the complete stream of bytes that represent:
-            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
-            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
             // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
             var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
             // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
@@ -82,7 +77,8 @@ namespace Licenta.ORM
                                 var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
                                 memoryStream.Close();
                                 cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                Array.Resize(ref plainTextBytes, decryptedByteCount);
+                                return plainTextBytes;
                             }
                         }
                     }
@@ -90,7 +86,7 @@ namespace Licenta.ORM
             }
         }
 
-        private static byte[] Generate256BitsOfRandomEntropy()
+        private byte[] Generate256BitsOfRandomEntropy()
         {
             var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
             using (var rngCsp = new RNGCryptoServiceProvider())
