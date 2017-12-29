93d7965742ad6a71c45033dd212f23154ee136d9
Bianca <Bianca@ASUS>
2017-12-29 18:26:55 +0200

    Load AES configuration from file

--- Licenta.ORM/AesConfiguration.cs
+++ Licenta.ORM/AesConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
@@ -12,15 +12,15 @@ namespace Licenta.ORM
         public PaddingMode Padding { get; set; }
         public byte[] EncryptedKey { get; set; }
         public byte[] EncryptedIv { get; set; }
-        public byte[] Key => Decrypt(EncryptedKey);
-        public byte[] Iv => Decrypt(EncryptedIv);
+        public byte[] Key => Decrypt(EncryptedKey, CertificateThumbprint);
+        public byte[] Iv => Decrypt(EncryptedIv, CertificateThumbprint);
 
-        private byte[] Decrypt(byte[] input)
+        private static byte[] Decrypt(byte[] input, string certificateThumbprint)
         {
             using (var certificateStore = new X509Store(StoreName.My, StoreLocation.LocalMachine))
             {
                 certificateStore.Open(OpenFlags.ReadOnly);
-                var certificate = certificateStore.Certificates.Find(X509FindType.FindByThumbprint, CertificateThumbprint, false)[0];
+                var certificate = certificateStore.Certificates.Find(X509FindType.FindByThumbprint, certificateThumbprint, false)[0];
 
                 using (RSA rsa = certificate.GetRSAPrivateKey())
                 {
@@ -29,12 +29,12 @@ namespace Licenta.ORM
             }
         }
 
-        private byte[] Encrypt(byte[] input)
+        private static byte[] Encrypt(byte[] input, string certificateThumbprint)
         {
             using (var certificateStore = new X509Store(StoreName.My, StoreLocation.LocalMachine))
             {
                 certificateStore.Open(OpenFlags.ReadOnly);
-                var certificate = certificateStore.Certificates.Find(X509FindType.FindByThumbprint, CertificateThumbprint, false)[0];
+                var certificate = certificateStore.Certificates.Find(X509FindType.FindByThumbprint, certificateThumbprint, false)[0];
 
                 using (RSA rsa = certificate.GetRSAPublicKey())
                 {
@@ -45,15 +45,37 @@ namespace Licenta.ORM
 
         public static AesConfiguration FromFile()
         {
-            return new AesConfiguration
+            if (File.Exists("crypto.config"))
             {
-                CertificateThumbprint = "9bbd172d892ea5f126afab5a7440aa5ae54eac87",
-                BlockSize = 256,
-                CipherMode = CipherMode.CBC,
-                Padding = PaddingMode.PKCS7,
-                EncryptedKey = Convert.FromBase64String("nX5RDbzhEQHdThBrCLR4WznVNLDhigMdaPYd9BVgcDpDFA0zlZqHm7UPF/rM/ITF9QA+e2IwKdvpOnTfcjU/qzsSkSSB/fwTgLv+glICV6ccoUWbKUAfRVseYlMiHqaC/2ad6DtEjqIJepcNjedGXLRUqs2etdnsLDfPLk3hJrnk6yk5fCHetUKpIFbrAgRG4/h9/WQ1BWR5C6O4Dl4MvhYrr1OAcB/MUWiY2cQhPoh1K840n5WYClmY+XRKJDGdICBg6lxuPskUMgkTyKgUgeIDg/Ze8q7BeZcGjdtSFVHDsU5Ush6XvI/9Uq9feZUvqVodsgCx/9cK07Y3fcpIxg=="),
-                EncryptedIv = Convert.FromBase64String("mFkSHqtrPrAXvfKIMRfuT4p2vTSxjwDM145nCxZGzL35xjSfBa5kUsm28AhxdfGtoDG9CQyvhtnvIlGLujuw+Uj535A0xDBaiML3yjbWWTcODbxIpfGwzt7F4UFALe16k0+FNl9vlHXF9QNpNmZrEqXI7Yxi4ElOGzwArqCZ5KOaYFrupf8N1cYa4Te31PBMB+hy1idEBbZ/aW8afPz/DQ4GsXapypAKov66zvBY3QnXXlihyKx2xAYkd0q5x+t18cRC4L5reIETkxU5umqmH64aYi567gR7RiMXQU6qJGSvKb8R06IVeNqaGU5fqtVJE+esjUJsMUa/uGfDqG6ocg==")
-            };
+                return XmlSerialization.Deserialize<AesConfiguration>(File.ReadAllBytes("crypto.config"));
+            }
+            else
+            {
+                string certificateThumbprint = "9bbd172d892ea5f126afab5a7440aa5ae54eac87";
+
+                var config = new AesConfiguration
+                {
+                    CertificateThumbprint = certificateThumbprint,
+                    BlockSize = 256,
+                    CipherMode = CipherMode.CBC,
+                    Padding = PaddingMode.PKCS7,
+                    EncryptedKey = Encrypt(GetRandomBytes(32), certificateThumbprint),
+                    EncryptedIv = Encrypt(GetRandomBytes(32), certificateThumbprint)
+                };
+
+                byte[] bytes = XmlSerialization.Serialize(config);
+                File.WriteAllBytes("crypto.config", bytes);
+
+                return config;
+            }
+        }
+
+        private static byte[] GetRandomBytes(int count)
+        {
+            byte[] randomBytes = new byte[count];
+            var randomBytesGenerator = new RNGCryptoServiceProvider();
+            randomBytesGenerator.GetNonZeroBytes(randomBytes);
+            return randomBytes;
         }
     }
 }
---
+++ Licenta.ORM/XmlSerialization.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Licenta.ORM
+{
+    class XmlSerialization
+    {
+        public static byte[] Serialize<T>(T item)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var xmlSerializer = new XmlSerializer(item.GetType());
+                xmlSerializer.Serialize(memoryStream, item);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static T Deserialize<T>(byte[] input)
+        {
+            using (var memoryStream = new MemoryStream(input))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                return (T)xmlSerializer.Deserialize(memoryStream);
+            }
+        }
+    }
+}
