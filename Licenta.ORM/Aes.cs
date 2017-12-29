e16cb87208bf78239729edc46a5d700e3921bafc
Bianca <Bianca@ASUS>
2017-12-29 16:55:57 +0200

    Refactoring

--- Licenta.ORM/Aes.cs
+++ Licenta.ORM/Aes.cs
@@ -14,42 +14,42 @@ namespace Licenta.ORM
 
         public byte[] Encrypt(byte[] plainTextBytes)
         {
-            using (var symmetricKey = new RijndaelManaged())
+            using (var algorithm = GetSymmetricAlgorithm())
+            using (var encryptor = algorithm.CreateEncryptor(configuration.Key, configuration.Iv))
+            using (var memoryStream = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
             {
-                symmetricKey.BlockSize = configuration.BlockSize;
-                symmetricKey.Mode = configuration.CipherMode;
-                symmetricKey.Padding = configuration.Padding;
-                using (var encryptor = symmetricKey.CreateEncryptor(configuration.Key, configuration.Iv))
-                using (var memoryStream = new MemoryStream())
-                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                {
-                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                    cryptoStream.FlushFinalBlock();
-                    memoryStream.Close();
-                    cryptoStream.Close();
-                    return memoryStream.ToArray();
-                }
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                memoryStream.Close();
+                cryptoStream.Close();
+                return memoryStream.ToArray();
             }
         }
 
         public byte[] Decrypt(byte[] cipherTextBytesWithSaltAndIv)
         {
-            using (var symmetricKey = new RijndaelManaged())
+            using (var algorithm = GetSymmetricAlgorithm())
+            using (var decryptor = algorithm.CreateDecryptor(configuration.Key, configuration.Iv))
+            using (var memoryStream = new MemoryStream(cipherTextBytesWithSaltAndIv))
+            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
             {
-                symmetricKey.BlockSize = configuration.BlockSize;
-                symmetricKey.Mode = configuration.CipherMode;
-                symmetricKey.Padding = configuration.Padding;
-                using (var decryptor = symmetricKey.CreateDecryptor(configuration.Key, configuration.Iv))
-                using (var memoryStream = new MemoryStream(cipherTextBytesWithSaltAndIv))
-                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-                {
-                    var plainTextBytes = new byte[cipherTextBytesWithSaltAndIv.Length];
-                    var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                    memoryStream.Close();
-                    cryptoStream.Close();
-                    return plainTextBytes;
-                }
+                var plainTextBytes = new byte[cipherTextBytesWithSaltAndIv.Length];
+                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return plainTextBytes;
             }
+        }
+
+        private SymmetricAlgorithm GetSymmetricAlgorithm()
+        {
+            return new RijndaelManaged
+            {
+                BlockSize = configuration.BlockSize,
+                Mode = configuration.CipherMode,
+                Padding = configuration.Padding
+            };
         }
     }
 }
